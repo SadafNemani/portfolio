d25367ec014060ae5e47d62380de3df39a7702a2
SadafNemani <sedefnemani@gmail.com>
2026-08-04 17:04:37 +0330

    Removed statistics data and component

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,20 +1,5 @@
-import { Characteristic, Statistic } from "@/types/about";
+import { Characteristic } from "@/types/about";
 import { BadgeCheck, BookOpen, Puzzle, Sparkles } from "lucide-react";
-
-export const statistics: Statistic[] = [
-  {
-    key: "projects",
-    value: "15+",
-  },
-  {
-    key: "experience",
-    value: "2+",
-  },
-  {
-    key: "clients",
-    value: "10+",
-  },
-];
 
 export const characteristics: Characteristic[] = [
   {
--- src/types/about.ts
+++ src/types/about.ts
@@ -1,10 +1,5 @@
 import type { LucideIcon } from "lucide-react";
 
-export interface Statistic {
-  key: string;
-  value: string;
-}
-
 export interface Characteristic {
   key: string;
   icon: LucideIcon;
